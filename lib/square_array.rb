314b576babae70e4a51f8eeb2a4a6fb9e40da159
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 13:48:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,5 +2,4 @@ def square_array(numbers)
   numbers = [1, 2, 3]
   while square_array[numbers] do
     puts square_array[numbers] 
-    square_array[numbers] sqrt
-end
+    square_array = numbers.sqrt
